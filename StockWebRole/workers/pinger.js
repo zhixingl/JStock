0f251be6eb3951dc2a13cf0286d36850ff734113
Zhixing Lv <cts-zhixingl@live.com>
2012-08-21 23:10:55 +0800

    0821

--- StockWebRole/workers/pinger.js
+++ StockWebRole/workers/pinger.js
@@ -29,4 +29,30 @@ module.exports.start = function(app, interval){
 
 	}, interval);
 
+	setInterval(function(){
+		var options = {
+		  host: 'jbuyer.cloudapp.net',
+		  // port: app.address().port || 80,
+		  path: '/ping',
+		  method: 'GET'
+		};
+		//console.log('start to send!');
+		var req = http.request(options, function(res) { 
+		  	res.on('end', function () {
+		    	util.log('The ping request has finished with status code ' + res.statusCode);
+		  	});
+		});
+
+		util.log(req._headers.host);
+		// util.log(req.path);
+
+		req.on('error', function(e) {
+		  util.log('problem with ping request: ' + e.message);
+		});
+
+		req.end();
+
+
+	}, interval);
+
 }
